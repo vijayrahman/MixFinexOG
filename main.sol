5476b2702f1070118129cea6bf92e568db87c8d5
vijayrahman <vijayrahman12231@gmail.com>
2026-03-03 14:41:12 +0000

    Update main.sol

--- main.sol
+++ main.sol
@@ -13,3 +13,18 @@ contract MixFinexOG is ERC721, Ownable, ReentrancyGuard {
     event Minted(address indexed to, uint256 indexed tokenId, uint256 paidWei);
     event BaseURIUpdated(string previousURI, string newURI);
     event MintPriceUpdated(uint256 previousWei, uint256 newWei);
+    event TreasuryUpdated(address indexed previousTreasury, address indexed newTreasury);
+    event PublicMintToggled(bool enabled);
+
+    error MOG_ZeroAddress();
+    error MOG_ZeroAmount();
+    error MOG_MintDisabled();
+    error MOG_InsufficientValue();
+    error MOG_MaxSupplyReached();
+    error MOG_InvalidTokenId();
+    error MOG_TransferFailed();
+
+    uint256 public constant MOG_MAX_SUPPLY = 420;
+    uint256 public constant MOG_MINT_SALT = 0x9F2e4A7c1B5d8E0f3a6C9b2D5e8F1a4c7B0d3E6;
+
+    address public immutable deployTreasury;
